95e1c235166bf55d8622e44588c7ea9ab82925a8
Alok Kumar <56478877+Alok5102R@users.noreply.github.com>
2024-02-14 00:31:43 +0530

    Responsiveness issue fixed

---
+++ static/JS/script.js
@@ -0,0 +1,19 @@
+// // Testing
+// let navbar = document.getElementsByClassName("username");
+// console.log(navbar[0].innerText);
+
+// // Freeing up space to be fit in mobile view
+// let device_width = window.innerWidth;
+// let device_height = window.innerHeight;
+
+// if(device_width<=650)
+// {
+//     document.getElementById("signup").innerText = "S";
+//     document.getElementById("signup").style.width = '1px';
+// }
+// else{
+//     document.getElementById("signup").innerText = "SignUp/SignIn";
+//     document.getElementById("signup").style.width = 'auto';
+// }
+
+// console.log(document.getElementById("signup").innerText);
